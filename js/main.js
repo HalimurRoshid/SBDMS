0ebc7b8b0196d92b4d895170908fbfc4a3a77807
Halimur Roshid Mahin <halimurroshidmahin@gmail.com>
2023-10-12 00:47:03 +0600

    updated vedio responsive

--- js/main.js
+++ js/main.js
@@ -102,10 +102,32 @@
         items: 1,
       },
       992: {
+        items: 3,
+      },
+    },
+  });
+  // Team carousel
+  $(".about-carousel, .related-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 45,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      992: {
         items: 2,
       },
     },
   });
+
 
   // Testimonials carousel
   $(".testimonial-carousel").owlCarousel({
